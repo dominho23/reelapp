2357c377cd7374fbcac8fd7b140fb64ac6fe8986
dominho23 <dominik_langenegger@bluewin.ch>
2026-04-29 09:37:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'reel-v5';
+const CACHE = 'reel-v8';
 const ASSETS = ['/reelapp/', '/reelapp/index.html'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
